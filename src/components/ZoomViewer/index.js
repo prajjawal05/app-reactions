c30324f6306da4bfaa91433e3f66dfe3ae46dcbb
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2020-06-12 01:07:15 +0530

    Tried to add some styles. didnt work

--- src/components/ZoomViewer/index.js
+++ src/components/ZoomViewer/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css";
 
 const ZoomViewer = ({meetingId = "2408823365"}) => (
     <iframe src={`https://zoom.us/wc/2408823365/join?prefer=0`} sandbox="allow-forms allow-scripts allow-same-origin" allow="microphone; camera;" width="100%"></iframe>
