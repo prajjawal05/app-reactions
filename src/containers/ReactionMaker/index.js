7584c38d53164a645f4f7919570cd4d80f36813f
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2020-06-20 18:30:11 +0530

    All changes done, no more other than config changes will be required

--- src/containers/ReactionMaker/index.js
+++ src/containers/ReactionMaker/index.js
@@ -15,9 +15,14 @@ if(!isMobile){
     REACTION_MAKER_STYLE = { ...REACTION_MAKER_STYLE, marginLeft:"10px" }
 }
 
+const reactionTypes = {...REACTION_TYPES};
+if (isMobile){
+    delete reactionTypes[REACTION_TYPES.LAUGH]
+}
+
 const ReactionMaker = ({ onReact, reactions }) => (
     <div style={REACTION_MAKER_STYLE}>
-        {Object.values(REACTION_TYPES).map(reactionType => (
+        {Object.values(reactionTypes).map(reactionType => (
             <ReactionButton key={reactionType} reactionCount={reactions[reactionType]} reactionType={reactionType} onClick={onReact}/>
         ))}
     </div>
