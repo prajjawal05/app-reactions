bf5a9ad6eaaa65172b826a8717d46250e052eebe
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2020-07-19 20:28:18 +0530

    Will now read meeting zoom id from url.

--- src/components/ZoomViewer/index.js
+++ src/components/ZoomViewer/index.js
@@ -1,11 +1,18 @@
-import React, {useMemo} from "react";
+import React from "react";
 import {withRouter} from "react-router-dom"
+import qs from "query-string"
 
-const ZoomViewer = ({match}) => {
-  const meetingId = useMemo(() => match.params.meetingId, []);
+//Todo: Separate MeetingIds
+
+const ZoomViewer = ({match: {params: {meetingId}}, location: {search}}) => {
+  let zoomUrl = `https://zoom.us/wc/join/${meetingId}?tk=&prefer=0`;
+  let pwd = qs.parse(search)["pwd"];
+  if (pwd) {
+    zoomUrl = zoomUrl + `&pwd=${pwd}`
+  }
   return (
     <iframe
-      src={`https://zoom.us/wc/join/${meetingId}?tk=&prefer=0&track_id=&meeting_result=&jmf_code=&wpk=&_x_zm_rtaid=bWMwMw-YTnKPTfUy17PxtA.1592163668617.72d53f29ee078ad1af175dd166907adf&_x_zm_rhtaid=61`}
+      src={zoomUrl}
       sandbox="allow-forms allow-scripts allow-same-origin"
       allow="microphone; camera;"
       width="100%"
