e4ef753780907b43ed290ba4046074f0b9fa2cfd
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2020-07-19 21:02:17 +0530

    Reverting back zoom url

--- src/components/ZoomViewer/index.js
+++ src/components/ZoomViewer/index.js
@@ -5,7 +5,7 @@ import qs from "query-string"
 //Todo: Separate MeetingIds
 
 const ZoomViewer = ({match: {params: {meetingId}}, location: {search}}) => {
-  let zoomUrl = `https://zoom.us/wc/join/${meetingId}?tk=&prefer=0`;
+  let zoomUrl = `https://zoom.us/wc/join/${meetingId}?tk=&prefer=0&track_id=&meeting_result=&jmf_code=&wpk=&_x_zm_rtaid=bWMwMw-YTnKPTfUy17PxtA.1592163668617.72d53f29ee078ad1af175dd166907adf&_x_zm_rhtaid=61`;
   let pwd = qs.parse(search)["pwd"];
   if (pwd) {
     zoomUrl = zoomUrl + `&pwd=${pwd}`
